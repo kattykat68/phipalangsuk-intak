1e46c91a2a15e08e5667a2968735e332c0a7264e
kattykat68 <kattykat.psy1@gmail.com>
2025-06-01 22:35:49 +0700

    Update form.js

--- form.js
+++ form.js
@@ -1,29 +1,22 @@
 document.getElementById("intake-form").addEventListener("submit", function(e) {
   e.preventDefault();
   const form = e.target;
-  const data = {
+  const params = new URLSearchParams({
     name: form.name.value,
     gender: form.gender.value,
     age: form.age.value,
     education: form.education.value,
     duration_amp: form.duration_amp.value,
     interest_quit: form.interest_quit.value
-  };
-
-  fetch("https://script.google.com/macros/s/AKfycbw0RRQ2ali5Le4sUcr9dA7S6DOJbYbOt-YcnJblFQS7thB8sOEUFOmByMSr3gh79Kq5/exec", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(res => {
-    if (res.ok) {
-      alert("ส่งข้อมูลเรียบร้อยแล้ว! 💚");
-      form.reset();
-    } else {
-      alert("ส่งข้อมูลไม่สำเร็จ โปรดลองอีกครั้งค่ะ");
-    }
-  }).catch(err => {
-    alert("เกิดข้อผิดพลาดในการส่งข้อมูล: " + err.message);
   });
+
+  fetch("https://script.google.com/macros/s/AKfycbw0RRQ2ali5Le4sUcr9dA7S6DOJbYbOt-YcnJblFQS7thB8sOEUFOmByMSr3gh79Kq5/exec?" + params)
+    .then(response => response.text())
+    .then(result => {
+      alert("ส่งข้อมูลสำเร็จแล้ว! 💚");
+      form.reset();
+    })
+    .catch(err => {
+      alert("เกิดข้อผิดพลาด: " + err.message);
+    });
 });
